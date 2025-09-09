646823c0149d6e75f1fbb49704ff502430106e15
namish <namish@covacap.com>
2025-09-10 01:51:05 +0530

    all conditions seems to working, code is messy needs cleanup. still messy but working.

--- src/controllers/identify.controller.ts
+++ src/controllers/identify.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import { db } from '../db/connection';
 import { Contact } from '../db/schema';
-import { and, eq, or, isNull } from 'drizzle-orm';
+import { and, eq, or, isNull, not, inArray } from 'drizzle-orm';
 
 export const identifyUser = async (req: Request, res: Response) => {
 	try {
-		console.log(req.body);
+		console.log(req.body, '\n<---------------------------------- request body');
 
 		const email = req.body.email;
 		const phoneNumber = req.body.phoneNumber;
@@ -13,29 +13,83 @@ export const identifyUser = async (req: Request, res: Response) => {
 		const fetchedRecords = await db
 			.select()
 			.from(Contact)
-			.where(
-				and(or(eq(Contact.email, email), eq(Contact.phoneNumber, phoneNumber)), eq(Contact.linkPrecedence, 'primary'))
-			);
-		console.log(fetchedRecords);
+			.where(or(eq(Contact.email, email), eq(Contact.phoneNumber, phoneNumber)));
+
+		console.log(fetchedRecords, '\n<---------------------------------- fetched records');
+
 		if (fetchedRecords.length === 0) {
 			await db.insert(Contact).values({
 				phoneNumber: phoneNumber || null,
 				email: email || null,
 				linkPrecedence: 'primary'
 			});
-		}
-		if (fetchedRecords.length == 1) {
-			await insertToDB(fetchedRecords[0], email, phoneNumber, 'secondary');
-		}
-		if (fetchedRecords.length > 1) {
-			const primaryRecord = fetchedRecords[0];
-			for (let i = 1; i < fetchedRecords.length; i++) {
+		} else {
+			const primaryRecords = fetchedRecords.filter((record) => record.linkPrecedence === 'primary');
+
+			let primaryRecordID: number;
+
+			if (primaryRecords.length > 0) {
+				primaryRecordID = primaryRecords.reduce((oldest, current) =>
+					current.createdAt < oldest.createdAt ? current : oldest
+				).id;
+			} else {
+				primaryRecordID = fetchedRecords[0].linkedId!;
+			}
+			const secondaryIds = fetchedRecords.filter((record) => record.id !== primaryRecordID).map((record) => record.id);
+
+			let emailExists,
+				phoneExists = false;
+			// check if the new email and phone number already exists. and adding flags for or condition
+			for (let record of fetchedRecords) {
+				// squashing nulls together like a, b to a, null or null, b
+				if (
+					(email === null || record.email === email) &&
+					(phoneNumber === null || record.phoneNumber === phoneNumber)
+				) {
+					const FinalRecords = await db
+						.select()
+						.from(Contact)
+						.where(or(eq(Contact.id, primaryRecordID), eq(Contact.linkedId, primaryRecordID)));
+					console.log(FinalRecords, '\n<---------------------------------- FinalRecords');
+
+					return res.status(200).json({
+						contact: {
+							primaryContatctId: primaryRecordID,
+							emails: [...new Set(FinalRecords.map((record) => record.email))],
+							phoneNumbers: [...new Set(FinalRecords.map((record) => record.phoneNumber))],
+							secondaryContactIds: FinalRecords.filter((record) => record.id !== primaryRecordID).map(
+								(record) => record.id
+							)
+						}
+					});
+				}
+				if (record.email === email) emailExists = true;
+				if (record.phoneNumber === phoneNumber) phoneExists = true;
+			}
+
+			if (secondaryIds.length > 0) {
 				await db
 					.update(Contact)
-					.set({ linkPrecedence: 'secondary', linkedId: primaryRecord.id })
-					.where(eq(Contact.id, fetchedRecords[i].id));
+					.set({ linkPrecedence: 'secondary', linkedId: primaryRecordID, updatedAt: new Date() })
+					.where(inArray(Contact.id, secondaryIds));
 			}
-			await insertToDB(primaryRecord, email, phoneNumber, 'secondary');
+			if (!emailExists || !phoneExists) {
+				await db.insert(Contact).values({
+					phoneNumber: phoneNumber || null,
+					email: email || null,
+					linkPrecedence: 'secondary',
+					linkedId: primaryRecordID
+				});
+			} else {
+				return res.status(200).json({
+					contact: {
+						primaryContatctId: primaryRecordID,
+						emails: [...new Set(fetchedRecords.map((record) => record.email))],
+						phoneNumbers: [...new Set(fetchedRecords.map((record) => record.phoneNumber))],
+						secondaryContactIds: secondaryIds
+					}
+				});
+			}
 		}
 
 		const FinalRecords = await db
@@ -43,68 +97,24 @@ export const identifyUser = async (req: Request, res: Response) => {
 			.from(Contact)
 			.where(or(eq(Contact.email, email), eq(Contact.phoneNumber, phoneNumber)));
 
-		console.log(FinalRecords);
+		console.log(FinalRecords, '\n<---------------------------------- FinalRecords');
+
+		let primaryRecordID = FinalRecords[0].linkPrecedence === 'primary' ? FinalRecords[0].id : FinalRecords[0].linkedId!;
+		const secondaryIds = FinalRecords.filter((record) => record.id !== primaryRecordID).map((record) => record.id);
+
 		res.status(200).json({
-			searched: { email, phoneNumber }
+			contact: {
+				primaryContatctId: primaryRecordID,
+				emails: [...new Set(FinalRecords.map((record) => record.email))],
+				phoneNumbers: [...new Set(FinalRecords.map((record) => record.phoneNumber))],
+				secondaryContactIds: secondaryIds
+			}
 		});
 	} catch (error) {
 		console.error('Database error:', error);
 		res.status(500).json({
 			error: 'Internal server error',
 			message: error instanceof Error ? error.message : 'Unknown error'
-		});
-	}
-};
-type RecordType = {
-	id: number;
-	phoneNumber: string | null;
-	email: string | null;
-	linkedId: number | null;
-	linkPrecedence: 'secondary' | 'primary';
-	createdAt: Date;
-	updatedAt: Date;
-	deletedAt: Date | null;
-};
-const insertToDB = async (
-	record: RecordType,
-	email: string | null,
-	phoneNumber: string | null,
-	linkPrecedence: 'secondary' | 'primary'
-) => {
-	if (linkPrecedence === 'secondary') {
-		if ((record.email !== email && email) || (record.phoneNumber !== phoneNumber && phoneNumber)) {
-			const whereConditions = [];
-			if (email) {
-				whereConditions.push(eq(Contact.email, email));
-			} else {
-				whereConditions.push(isNull(Contact.email));
-			}
-			if (phoneNumber) {
-				whereConditions.push(eq(Contact.phoneNumber, phoneNumber));
-			} else {
-				whereConditions.push(isNull(Contact.phoneNumber));
-			}
-
-			const existingRecord = await db
-				.select()
-				.from(Contact)
-				.where(and(...whereConditions));
-
-			if (existingRecord.length === 0) {
-				await db.insert(Contact).values({
-					phoneNumber: phoneNumber || null,
-					email: email || null,
-					linkPrecedence: linkPrecedence,
-					linkedId: record.id
-				});
-			}
-		}
-	} else {
-		// not useing the explicit check because if adding primary then it must be a new entry. also this condition is already explicitly tested before calling api
-		await db.insert(Contact).values({
-			phoneNumber: phoneNumber || null,
-			email: email || null,
-			linkPrecedence: linkPrecedence
 		});
 	}
 };
